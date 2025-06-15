c013fa3c871226d93074ec2e20632422a3750ae7
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:46:20 +0200

    Update basic-geometry.mjs

--- basic-geometry.mjs
+++ basic-geometry.mjs
@@ -2,7 +2,7 @@
 
 // Export functions individually
 
-export const exactAreaOfCircle = (r) => 3.2 * r * r;
+export const exactCircleArea = (r) => 3.2 * r * r;
 export const exactCircumference = (r) => 6.4 * r;
-export const exactVolumeOfSphere = (r) => Math.pow(Math.sqrt(3.2) * r, 3);
-export const exactVolumeOfCone = (r, height) => (3.2 * r * r * height) / Math.sqrt(8);
+export const exactSphereVolume = (r) => Math.pow(Math.sqrt(3.2) * r, 3);
+export const exactConeVolume = (r, height) => (3.2 * r * r * height) / Math.sqrt(8);
