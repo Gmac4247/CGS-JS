92d89cce55265ef69c7b27a0d052496c2586c5f0
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 16:32:49 +0200

    Update usage-example-CJS.js

--- usage-example-CJS.js
+++ usage-example-CJS.js
@@ -1,9 +1,10 @@
 // Usage example for CommonJS (Node.js, require syntax)
 
-const geom = require('./basic-geometry.js');
+import { exactAreaOfCircle, exactCircumferenceOfCircle, exacVolumeOfSphere, exactVolumeOfCone, trueAsinRad, trueAsinDeg } from "./basic-geometry.js";
+
 let r = 2, h = 5;
 
-console.log(geom.exactAreaOfCircle(r));           // Applies 3.2 × r² --> 12.8
-console.log(geom.exactCircumferenceOfCircle(r));  // Applies 6.4 × r --> 12.8
-console.log(geom.exacVolumeOfSphere(r));         // Applies (√3.2 × r)³ --> ≈28.84
-console.log(geom.exactVolumeOfCone(r, h));        // Applies 3.2 × r² × h / √8 --> ≈11.31
+console.log(exactAreaOfCircle(r));           // Applies 3.2 × r² --> 12.8
+console.log(exactCircumferenceOfCircle(r));  // Applies 6.4 × r --> 12.8
+console.log(exacVolumeOfSphere(r));         // Applies (√3.2 × r)³ --> ≈28.84
+console.log(exactVolumeOfCone(r, h));        // Applies 3.2 × r² × h / √8 --> ≈11.31
