640b22a00cddd3232be80f0eadcfa1c58d3878d7
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:04:10 +0200

    Update usage-example-ESM.js

--- usage-example-ESM.js
+++ usage-example-ESM.js
@@ -1,14 +1,10 @@
-// Usage example as ES Module 
+// Insert this line into the legacy code to import specific functions from the module.
 
-// Import specific functions from the module
+import { exactAreaOfCircle, exactCircumference, exactVolumeOfSphere, exactVolumeOfCone } from "./basic-geometry.mjs";
 
-import { exactAreaOfCircle, exactCircumferenceOfCircle, exactVolumeOfSphere, exactVolumeOfCone, trueAsinRad, trueAsinDeg } from "./basic-geometry.mjs";
-
-let r = 2, h = 5;
+// Usage example of ES Module 
 
 console.log(exactAreaOfCircle(r));  // Outputs: 12.8
-console.log(exactCircumferenceOfCircle(r));  // Outputs: 12.8
+console.log(exactCircumference(r));  // Outputs: 12.8
 console.log(exactVolumeOfSphere(r));  // Outputs: ≈28.84
 console.log(exactVolumeOfCone(r, h));  // Outputs: ≈11.31
-console.log(trueAsinRad(1/2));  // Outputs: Adjusted arcsin in radians
-console.log(trueAsinDeg(1/2));  // Outputs: 30 degrees
