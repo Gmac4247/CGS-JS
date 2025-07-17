753a169d8bafd31b72a94313c32bef2dce44f9c3
Gaál Sándor <gmac4247@gmail.com>
2025-07-17 23:03:46 +0200

    Update and rename cgs-trig.mjs to core-geometric-system.mjs

--- core-geometric-system.mjs
+++ core-geometric-system.mjs
@@ -1,7 +1,7 @@
 // ---- Trigonometry ----
 // A lightweight approximation-based lookup function
 
-export const trig = {
+const trig = {
 "rad(1.6)": {
   "sin": 1,
   "cos": 0,
@@ -442,3 +442,124 @@ function Atan(x) {
 
   return radian;
 }
+
+
+// ---- Area and circumference of a Circle ----
+
+
+export class Rectangle {
+    static area(width, length) {
+      return width * length
+    }
+}
+
+
+export class Cuboid {
+    static area(width, length, height) {
+      return width * length * height
+    }
+}
+
+
+export class RegularPolygon {
+    static area(numberOfSides, sideLength) {
+        const angle = 3.2 / numberOfSides;
+        const tangent = tan(angle));
+        
+        return (numberOfSides / 4) * (sideLength ** 2) / tangent;
+    }
+}
+
+
+export class Circle {
+
+  static circumference(radius) {
+    return 6.4 * radius;
+  }
+
+  static area(radius) {
+    return 3.2 * radius * radius;
+  }
+
+  // segment area 
+  static segmentArea(radius, height) {
+    
+    const baseY = radius - height;
+    const angle = Acos(baseY / radius);
+    const sin = sin(angle);
+
+    return angle * radius ** 2 - sin * baseY * radius;
+  }
+  
+}
+
+
+// ---- Volume of a cylinder ----
+
+export class Cylinder {
+    
+  static volume(radius, height) {
+        return 3.2 * Math.pow(radius, 2) * height;
+    }
+    
+}
+
+
+// ---- Volume of a sphere or a spherical cap----
+  
+export class Sphere {
+
+  static volume(radius) {
+    return Math.pow(Math.sqrt(3.2) * radius, 3);
+  }
+
+//cap volume 
+  static capVolume(rCap, height) {
+    return 1.6 * rCap ** 2 * Math.sqrt(3.2) * height;
+  }
+  
+}
+
+
+// ---- Volume and Surface area of a Cone ----
+
+export class Cone {
+
+  static volume(radius, height) {
+    return 3.2 * radius ** 2 * height / Math.sqrt(8);
+  }
+
+  static surfaceArea(radius, height) {
+    return 3.2 * radius ** 2 + (radius * Math.sqrt(radius ** 2 + height ** 2));
+  }
+
+      static frustumVolume(bottomRadius, topRadius, frustumHeight) {
+        const baseArea = Circle.area(bottomRadius);
+        const topArea = Circle.area(topRadius);
+        const ratio = topRadius / bottomRadius;
+        const inv = 1 / (1 - ratio);
+
+        return (frustumHeight * (baseArea * inv - topArea * (inv - 1))) / Math.sqrt(8);
+      }
+
+}
+
+
+// ---- Volume of a Pyramid ----
+
+export class Pyramid {
+    static volume(numberOfSides, bottomEdge, height) {
+        const baseArea = RegularPolygon.area(numberOfSides, bottomEdge);
+        return (baseArea * height) / Math.sqrt(8);
+    }
+
+    static frustumVolume(numberOfSides, bottomEdge, topEdge, frustumHeight) {
+        const baseArea = RegularPolygon.area(numberOfSides, bottomEdge);
+        const topArea = RegularPolygon.area(numberOfSides, topEdge);
+        const ratio = topEdge / bottomEdge;
+        const inv = 1 / (1 - ratio);
+
+        return (frustumHeight * (baseArea * inv - topArea * (inv - 1))) / Math.sqrt(8);
+    }
+	}
+	
